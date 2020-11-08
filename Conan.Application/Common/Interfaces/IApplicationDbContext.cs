6767be5c8f534622a34178dba92c1ab464052424
Ray Brown-Amory <r.brown-amory@sky.com>
2020-11-08 22:18:50 +0000

    Creating the Clean Archtechure

---
+++ Conan.Application/Common/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Conan.Application.Common.Interfaces
+{
+   public interface IApplicationDbContext
+    {
+        
+    }
+}
